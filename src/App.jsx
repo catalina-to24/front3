fe58bb81589a18f68cde115904134cedf0be1446
catalina-to24 <catalinatempraotero@gmail.com>
2024-05-06 22:57:18 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hello from './Components/Hello'
+
+function App() {
+  
+
+  return (
+    <>
+      <Hello nombre = 'Lucas'/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hello.jsx
@@ -0,0 +1,11 @@
+
+const Hello = ({nombre}) => {
+  return (
+    <>
+    <h3>Hola {nombre}</h3>
+    </>
+  )
+}
+
+export default Hello
+
